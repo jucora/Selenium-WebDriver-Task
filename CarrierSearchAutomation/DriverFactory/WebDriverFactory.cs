9cad2dcafa2e064e5496499e0968102f3d7b070a
Julian Belmonte <artjulius@hotmail.com>
2025-11-09 18:07:00 -0500

    Add Driver Factory

---
+++ CarrierSearchAutomation/DriverFactory/WebDriverFactory.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace CarrierSearchAutomation.Drivers
+{
+    public static class WebDriverFactory
+    {
+        public static IWebDriver Create(string browser = "firefox")
+        {
+            IWebDriver driver = browser.ToLower() switch
+            {
+                "firefox" => new FirefoxDriver(),
+                _ => new ChromeDriver()
+            };
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // Espera implícita
+            return driver;
+        }
+    }
+}
